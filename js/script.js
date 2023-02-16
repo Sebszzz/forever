3e62d00dab4990c0537d320691f6d976ffc7ca79
Sebi <124313209+Sebszzz@users.noreply.github.com>
2023-02-16 17:49:47 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,22 @@
 
-// Get all the section elements
-var sections = document.querySelectorAll('.section');
+let popupShown = false;
 
+function showPopup() {
+  document.getElementById("popup1").style.display = "block";
+  popupShown = true;
+}
+
+function chooseOption(option) {
+  if (option === "option1") {
+    alert("You chose option 1");
+  } else if (option === "option2") {
+    alert("You chose option 2");
+  }
+  document.getElementById("popup1").style.display = "none";
+}
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 500 && !popupShown) {
+    showPopup();
+  }
+});
